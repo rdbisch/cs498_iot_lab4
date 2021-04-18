9635d23685b91e7b51174ca617cb2e8ba0429983
Rick Bischoff <rdbisch@gmail.com>
2021-04-18 08:56:41 -0500

    working part 4.2 greengrass

---
+++ greengrass_module4/publish.sh
@@ -0,0 +1,3 @@
+python3 basicDiscovery.py --endpoint a9ejcm75pxqdq-ats.iot.us-east-2.amazonaws.com --rootCA ../AmazonRootCA1.pem.txt --cert 05005f169e.cert.pem --key 05005f169e.private.key --thingName HelloWorld_Publisher --topic 'hello/world/pubsub' --mode publish --data ../data/vehicle0.csv
+
+
---
+++ greengrass_module4/sub.sh
@@ -0,0 +1,3 @@
+python3 basicDiscovery.py --endpoint a9ejcm75pxqdq-ats.iot.us-east-2.amazonaws.com --rootCA ../AmazonRootCA1.pem.txt --cert d0c1e4e519.cert.pem --key d0c1e4e519.private.key --thingName HelloWorld_Subscriber --topic 'hello/world/pubsub' --mode subscribe
+
+
